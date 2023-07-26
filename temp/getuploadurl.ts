83199570dff8ddc31b7f7351be811462e89077f3
Jay Boricha <jayboricha707@gmail.com>
2023-07-26 20:32:26 +0530

    configured the initial configurations

--- temp/getuploadurl.ts
+++ temp/getuploadurl.ts
@@ -1,15 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 // import { NextRequest } from 'next/server';
 import { DeleteObjectCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 // import formidable from "formidable";
 import {IncomingForm} from 'formidable';
 
-// export const config = {
-//   api: {
-//     bodyParser: false
-//   }
-// };
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   // const response = await fetch(
@@ -20,13 +19,24 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   //   }
   // )
 
-  const { fileType, fileName } = JSON.parse(req.body);
-  console.log("incoming request", req.body, {fileType, fileName});
   const containerName = process.env.CONTAINER_NAME;
   const blobName = new Date().getTime();
+  const form = new IncomingForm();
+  form.parse(req, async (err, fields, files) => {
+
+    // const formData = await req.formData();
+
+    const file: any = files.file?.[0]; //formData.get('file');
+    console.log("file data", Array.isArray(files.file), {file});
+
+    if (!file) {
+        res.json({status: 'error', message: 'Invalid File'});
+    }
+
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
 
   const s3Client = new S3Client({
-    // forcePathStyle: false, // Configures to use subdomain/virtual calling format.
     endpoint: `https://${process.env.SPACES_ENDPOINT}`,
     region: 'blr1', // Replace this with the appropriate region for your Space
     credentials: {
@@ -39,29 +49,24 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   const params = {
     Bucket: process.env.SPACES_BUCKET_NAME, // The path to the directory you want to upload the object to, starting with your Space name.
-    Key: `${containerName}/${fileName}`, // Object key, referenced whenever you want to access this file later.
+    Key: `${containerName}/${blobName}`, // Object key, referenced whenever you want to access this file later.
+    Body: buffer, // The object's contents as a buffer.
     ACL: 'public-read', // Defines ACL permissions, such as private or public.
-    ContentType: fileType, // Content type of the file. // Content length of the file.
+    ContentType: file.type || file.mimetype, // Content type of the file.
+    ContentLength: file.size, // Content length of the file.
   };
 
+  // Use S3Client to upload the object
+  const data = await s3Client.send(new PutObjectCommand(params));
   // Get the public URL of the uploaded file
-  // const url = `https://${process.env.SPACES_BUCKET_NAME}.${process.env.SPACES_ENDPOINT}/${containerName}/${blobName}`;
+  const url = `https://${process.env.SPACES_BUCKET_NAME}.${process.env.SPACES_ENDPOINT}/${containerName}/${blobName}`;
 
-  const uploadURL = await getSignedUrl(s3Client, new PutObjectCommand(params), {expiresIn: 1 * 60});
-  console.log('Successfully uploaded object:', uploadURL);
-  console.log(fileName);
-  console.log(req.body);
+  console.log('Successfully uploaded object:', url);
   res
     .status(200)
-    .json({ 
-      status: 'success', 
-      payload: { 
-        uploadURL,
-        blobName: fileName
-      } 
-    });
+    .json({ status: 'success', payload: { url: url } });
 
-  // });
+  });
   
 
 }
